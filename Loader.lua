6be022b57d9ac6e013decaa2425f53e1c3721079
Chrono <150987804+ChronoAcceleration@users.noreply.github.com>
2024-10-01 16:26:38 -0700

    Update Loader.lua
    Add initial loader script for Hotel ++ Project
    
    - Implement loadCode function to fetch and execute remote scripts
    - Support for running scripts in a new thread or the main thread
    - Load Backend/NodeConverter.lua and QOL/Ambient.lua scripts

--- Loader.lua
+++ Loader.lua
@@ -9,3 +9,15 @@ _G.DEBUG_HOTELPLUSPLUSPLUS (self explanatory lol)
 
 --]]
 
+local function loadCode(URL: string, NewThread: boolean): ()
+    local Raw = game:HttpGet(URL)
+
+    if NewThread then
+        task.spawn(loadstring(Raw, "Hotel++"))
+    else
+        loadstring(Raw, "Hotel++")()
+    end
+end
+
+loadCode("https://raw.githubusercontent.com/ChronoAcceleration/Hotel-Plus-Plus/refs/heads/main/Backend/NodeConverter.lua") -- Backend/NodeConverter.lua
+loadCode("https://raw.githubusercontent.com/ChronoAcceleration/Hotel-Plus-Plus/refs/heads/main/QOL/Ambient.lua", true)  -- QOL/Ambient.lua (Includes While Loop)
