3d4d6116ad5b8478b69ed460d35f47040a39e0cb
Chrono <150987804+ChronoAcceleration@users.noreply.github.com>
2024-10-01 16:21:04 -0700

    Create NodeConverter.lua

---
+++ Backend/NodeConverter.lua
@@ -0,0 +1,37 @@
+-- NodeConverter.lua
+-- [Comet Development] By Chrono
+-- This file is part of the Hotel ++ Project.
+
+local CurrentRooms = workspace:WaitForChild("CurrentRooms")
+
+local function convertToLegacy(Room: Model): ()
+    if Room:HasTag("Converted") then
+        return nil
+    end
+
+    local PathfindNodes = Room:FindFirstChild("PathfindNodes")
+
+    if not PathfindNodes then
+        warn("PathfindNodes not found in room: " .. Room.Name)
+        return nil
+    end
+
+    local LegacyNodes = PathfindNodes:Clone()
+    LegacyNodes.Name = "Nodes"
+    LegacyNodes.Parent = Room
+    Room:AddTag("Converted")
+
+    if _G.DEBUG_HOTELPLUS then
+        warn("Converted PathfindNodes to Nodes in room: " .. Room.Name)
+    end
+end
+
+CurrentRooms.ChildAdded:Connect(
+    function(Room: Model): ()
+        convertToLegacy(Room)
+    end
+)
+
+for Index, Room in CurrentRooms:GetChildren() do
+    convertToLegacy(Room)
+end
